e9c2f8af2ed27a45035e741e21c722944093a16d
yery.encinas <115434248+yeryencinas98@users.noreply.github.com>
2025-05-29 20:36:51 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('¡Hola, Mundo!'));
+app.listen(3000, () => console.log('Servidor ejecutándose en el puerto 3000'));
